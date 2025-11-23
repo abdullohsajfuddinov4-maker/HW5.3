3dd329508af25c3edfcdc2951b21b8d789f15fe8
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-11-23 18:25:05 +0500

    2masala

--- HW5.3.py
+++ HW5.3.py
@@ -13,4 +13,27 @@ def son (n):
 print(son ([1,3,5,5,-500,54,-5]))
 '''
 #2
+'''
+
+def matin(f):
+    def w(a):
+        s = []
+        for i in a:
+            try:
+                if i.isalpha():
+                    s.append(i)
+            except AttributeError:
+                pass
+        return f(s)
+    return w
+
+
+@matin
+def str_func(s):
+    return s
+
+
+print(str_func([5, 4, 1, 'dhgh', 656, 'dh']))
+'''
+
 
