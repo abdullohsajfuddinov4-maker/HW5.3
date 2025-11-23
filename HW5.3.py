984b766266fdbbc04d75181e6a0724a01bbfdc60
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-11-23 18:07:05 +0500

    1 masala

---
+++ HW5.3.py
@@ -0,0 +1,16 @@
+#1
+'''
+def manfi_son(f):
+    def w(a):
+        n = [i for i in a if i < 0]
+        return f(n)
+    return w 
+
+@manfi_son
+def son (n):
+    return n
+
+print(son ([1,3,5,5,-500,54,-5]))
+'''
+#2
+
